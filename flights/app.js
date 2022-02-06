278a882e138d82b5ec4effd81800bcc1fc551876
ryanrrogers <ryan.rogers.developer@gmail.com>
2022-02-05 22:37:40 -0500

    updated branch, create flight response

---
+++ flights/app.js
@@ -0,0 +1,31 @@
+require('dotenv').config({path: './KEYS.env'})
+
+let Amadeus = require('amadeus')
+
+var amadeus = new Amadeus({
+	clientId: process.env.CLIENT_ID,
+	clientSecret: process.env.CLIENT_SECRET
+})
+
+// amadeus.shopping.hotelOffers.get({
+// 	cityCode: 'NYC'
+// }).then(function(response){
+// 	console.log(response.data)
+// }).catch(function(responseError){
+// 	console.log(responseError.code)
+// })
+
+amadeus.shopping.flightOffersSearch.get({
+    originLocationCode: 'SYD',
+    destinationLocationCode: 'BKK',
+    departureDate: '2022-04-04',
+    adults: '2'
+}).then(function(response){
+    return amadeus.shopping.flightOffers.prediction.post(
+      JSON.stringify(response)
+    );
+}).then(function(response){
+    console.log(response.data);
+}).catch(function(responseError){
+    console.log(responseError);
+});
