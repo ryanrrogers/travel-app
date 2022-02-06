5dbf5dd4701e8fcbe329447c2052cb0c44c8bb9b
ryanrrogers <ryan.rogers.developer@gmail.com>
2022-02-05 22:39:06 -0500

    remove deprecated files

--- hotels/app.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config({path: './KEYS.env'})
-
-let Amadeus = require('amadeus')
-
-var amadeus = new Amadeus({
-	clientId: process.env.CLIENT_ID,
-	clientSecret: process.env.CLIENT_SECRET
-})
