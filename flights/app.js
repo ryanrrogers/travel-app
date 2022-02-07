1a63e179fc7e3531af308d95b173bdd504077cf5
ryanrrogers <ryan.rogers.developer@gmail.com>
2022-02-07 18:46:09 -0500

    seperate api functions

--- flights/app.js
+++ flights/app.js
@@ -1,23 +1,3 @@
-require('dotenv').config({path: './KEYS.env'})
+let flights = require('./classes/flights')
 
-let Amadeus = require('amadeus')
-
-var amadeus = new Amadeus({
-	clientId: process.env.CLIENT_ID,
-	clientSecret: process.env.CLIENT_SECRET
-})
-
-amadeus.shopping.flightOffersSearch.get({
-    originLocationCode: 'SYD',
-    destinationLocationCode: 'BKK',
-    departureDate: '2022-04-04',
-    adults: '2'
-}).then(function(response){
-    return amadeus.shopping.flightOffers.prediction.post(
-      JSON.stringify(response)
-    );
-}).then(function(response){
-    console.log(response.data);
-}).catch(function(responseError){
-    console.log(responseError);
-});
+flights.test()
