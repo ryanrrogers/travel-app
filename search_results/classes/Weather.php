64f78fa638181c89990b1b0356e47706c5ca81dc
ryanrrogers <ryan.rogers.developer@gmail.com>
2022-04-03 17:25:42 -0400

    add weather

---
+++ search_results/classes/Weather.php
@@ -0,0 +1,35 @@
+<?php
+
+class Weather
+{
+    public $corrlon;
+    public $corrlat;
+    public $weatherid;
+    public $weathermain;
+    public $weatherdescription;
+    public $basestationsmaintemp;
+    public $basestationsmaintempfeels_like;
+    public $basestationsmaintemp_min;
+    public $basestationsmaintemp_max;
+    public $basestationpressure;
+    public $basestationshumidity;
+    public $visibility;
+    public $windspeed;
+    public $winddeg;
+    public $cloudsall;
+    public $sunrise;
+    public $sunset;
+    public $timezone;
+    public $errors = [];
+
+    public static function getAll($conn)
+    {
+        $sql = "SELECT *
+                FROM Weather
+                ORDER BY id;";
+        
+        $results = $conn->query($sql);
+
+        return $results->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
--- search_results/index.php
+++ search_results/index.php
@@ -5,9 +5,28 @@ require 'includes/init.php';
 $conn = require 'includes/db.php';
 
 $flights = Flight::getAll($conn);
+
+$weather = Weather::getAll($conn);
 ?>
 
-<?php if (empty($flights)) : ?>
+<?php if (empty($weather)) : ?>
+    <p>No weather found!</p>
+<?php else : ?>
+    <ul>
+        <?php foreach ($weather as $weather_instance) : ?>
+            <li>
+                <ol>
+                    <li><?= htmlspecialchars($weather_instance['weathermain']); ?></li>
+                    <li><?= htmlspecialchars($weather_instance['weatherdescription']); ?></li>
+                    <li><?= htmlspecialchars($weather_instance['basestationshumidity']); ?></li>
+                    <li><?= htmlspecialchars($weather_instance['basestationsmaintempx']); ?></li>
+                </ol>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
+
+<?php if (empty($flights) : ?>
     <p>No flights found!</p>
 <?php else : ?>
     <ul>
