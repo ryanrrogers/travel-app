e2bb343da4607d0b60b0324d810b9044e8b25554
vuongev12 <90983912+vuongev12@users.noreply.github.com>
2022-04-07 18:56:44 -0400

    Create MyApiKey.py

---
+++ restaurants/MyApiKey.py
@@ -0,0 +1,7 @@
+api_key = 'pOFwhJS51KoAREGxWwyyFpYdOXWgM3BMfPHddwXzF6KDkpwTnbEQv4x7EuZwLLUCVKMB08HPxmlZdJ_zBafGrrgigJC9OWy-9hmwUd9W1Jbz3loV1xv09u6HpnwFYnYx'
+
+def get_api_key():
+    return api_key
+    
+    
+    
