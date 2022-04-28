a262531f7f81fe2376b431e19d7e2c6c92f62a45
ryanrrogers <ryan.rogers.developer@gmail.com>
2022-04-28 19:26:08 -0400

    my changes

--- views/includes/url.php
+++ views/includes/url.php
@@ -2,6 +2,6 @@
 
 function redirect($path)
 {
-    header("Location: http://localhost:8080/Travel-App/" . $path . '.php');
+    header("Location: http://localhost/Travel-App/" . $path . '.php');
     exit;
 }
